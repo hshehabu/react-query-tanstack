7fdb62efb8bdb07de0c9f3d3046eff7c35230d80
hshehabu <hamzashehabu457@gmail.com>
2024-10-09 17:16:34 +0300

    from the scratch

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,57 +1,15 @@
 import React from "react";
-import { useState } from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { fetchTodos } from "./api";
+export default function Demo () {
 
-import { addTodo, fetchTodos } from "./api";
-import TodoCard from "./components/TodoCard";
-
-export default function Demo() {
-  const queryClient = useQueryClient();
-
-  const [search, setSearch] = useState("");
-  const [title, setTitle] = useState("");
-
-  const { data: todos, isLoading } = useQuery({
-    queryKey: ["todos", { search }],
-    queryFn: () => fetchTodos(search),
-    staleTime: Infinity,
-  });
-
-  const { mutateAsync: addTodoMutation } = useMutation({
-    mutationFn: addTodo,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["todos"] });
-    },
-  });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <button
-          onClick={async () => {
-            try {
-              await addTodoMutation({ title });
-              setTitle("");
-            } catch (e) {
-              console.log(e);
-            }
-          }}
-        >
-          Add Todo
-        </button>
-      </div>
-      {todos?.map((todo) => (
-        <TodoCard key={todo.id} todo={todo} />
-      ))}
-    </div>
-  );
+    const {data : todos , isLoading } = useQuery({
+        queryFn: ()=>fetchTodos() ,
+        queryKey: ["todos"]
+    });
+    return (
+        <div>
+            <b>React query</b>
+        </div>
+    )
 }
